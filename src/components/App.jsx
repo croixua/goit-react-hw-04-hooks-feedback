fd3dfd97ac2cf31ddfa216448912e2b1cd93522e
Anton Kit <croixua@gmail.com>
2022-06-05 21:22:13 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,66 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import Container from './Container/Container';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  onBtnClick = e => {
+  const onBtnClick = e => {
     const value = e.currentTarget.textContent.toLowerCase();
 
-    this.setState(prevState => {
-      return {
-        [value]: prevState[value] + 1,
-      };
-    });
+    switch (value) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return +((this.state.good * 100) / this.countTotalFeedback()).toFixed(2);
+  const countPositiveFeedbackPercentage = () => {
+    return +((good * 100) / countTotalFeedback()).toFixed(2);
   };
 
-  render() {
-    return (
-      <Container>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            onBtnClick={this.onBtnClick}
+  return (
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          onBtnClick={onBtnClick}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {bad === 0 && good === 0 && neutral === 0 ? (
+          <Notification message={'There is no feedback'} />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            percentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-
-        <Section title="Statistics">
-          {this.state.bad === 0 &&
-          this.state.good === 0 &&
-          this.state.neutral === 0 ? (
-            <Notification message={'There is no feedback'} />
-          ) : (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              percentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </Section>
-      </Container>
-    );
-  }
+        )}
+      </Section>
+    </Container>
+  );
 }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
 import s from './FeedbackOptions.module.css';
 
-class FeedbackOptions extends Component {
-  render() {
-    return (
-      <div className={s.wrapper}>
-        <button type="button" className={s.btn} onClick={this.props.onBtnClick}>
-          Good
-        </button>
-        <button type="button" className={s.btn} onClick={this.props.onBtnClick}>
-          Neutral
-        </button>
-        <button type="button" className={s.btn} onClick={this.props.onBtnClick}>
-          Bad
-        </button>
-      </div>
-    );
-  }
+export default function FeedbackOptions({ onBtnClick }) {
+  return (
+    <div className={s.wrapper}>
+      <button type="button" className={s.btn} onClick={onBtnClick}>
+        Good
+      </button>
+      <button type="button" className={s.btn} onClick={onBtnClick}>
+        Neutral
+      </button>
+      <button type="button" className={s.btn} onClick={onBtnClick}>
+        Bad
+      </button>
+    </div>
+  );
 }
-
-export default FeedbackOptions;
